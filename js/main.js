15c1db58b27759e60d0b1b7318bdc7c07629ee28
CrSjimo <crs_16423@outlook.com>
2018-06-24 20:31:23 +0800

    1

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	var list;
-	$.get("https://mhw-app-article.mhwco.org/list/list.txt",function(data,status){
-		list=data.split("\n");
-	});
-	$("#content").append("
-})
