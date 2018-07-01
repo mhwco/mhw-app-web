d89013c3a361fd3180a5c1b21aca55fdca79a8f3
CrSjimo <crs_16423@outlook.com>
2018-07-01 20:36:17 +0800

    1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
+function addcard(title,detail,img,author,avator,date){
+	"use strict";
+	$("#content").append('<div class="card"><div class="title-bar"><img class="avator" alt="作者头像" src="'+avator+'"/><span class="author">'+author+'</span><span class="date">'+date+'</span></div><img class="article-img" alt="我是文章缩略图" src="'+img+'"/><h2 class="article-title">'+title+'</h2><div class="article-detail">'+detail+'</div></div>');
+}
 $(document).ready(function(){
-	var pos = 0;
-	$.get("https://mhw-app-article.mhwco.org/list/list.txt",function(data,status){
-		var list=data.split("\r\n");
-		if(list.length<=20){
-			for(var i=0;i<list.length;i++){
-				var x=list[i];
-				$("#t").load("https://mhw-app-article.mhwco.org/article/"+x+"/manifest.json");
-				
-			}
-		}
-	});
-})
+	"use strict";
+	/*addcard("test","testtest","https://www.mhwco.org/image/mhwco-logo/mhwco.svg","root","https://www.mhwco.org/image/mhwco-logo/mhwco-min.svg","1970-1-1");*/
+	$("#t").load("https://mhw-app-article.mhwco.org/list/list.txt");
+	
+});
