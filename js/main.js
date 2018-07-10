48809c27c127ca9cd0dfaa1011c30a9993dfca43
CrSjimo <crs_16423@outlook.com>
2018-07-10 20:42:08 +0800

    1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var atsroot="https://mhw-app-article.mhwco.org/";
 function addcard(title,detail,img,author,avatar,date,href){
+	"use strict";
 	$("#content").append('<div class="card" onclick="location.href=\''+href+'\'"><div class="title-bar"><img class="avator" alt="作者头像" src="'+avatar+'"/><span class="author">'+author+'</span><span class="date">'+date+'</span></div><img class="article-img" alt="我是文章缩略图" src="'+img+'"/><h2 class="article-title">'+title+'</h2><div class="article-detail">'+detail+'</div></div></a>');
 }
 function jsonParse(path,isCard){//向文章列表增加卡片
-	$.get(path+"manifest.json",function(mf,status){
+	"use strict";
+	$.get(path+"manifest.json",function(mf){
 		if(isCard){
 			addcard(mf.title,mf.detail,mf.image,mf.author,atsroot+"avatar/"+mf.author+".png",mf.date,path+"index.html");
 		}else{
@@ -11,7 +13,8 @@ function jsonParse(path,isCard){//向文章列表增加卡片
 		}
 	});
 }
-function jsonMapping(liststring){//分割list字符串并分别get
+function jsonMapping(liststring,isCard){//分割list字符串并分别get
+	"use strict";
 	var list=liststring.split("\r\n");
 	var t=list.length<3?list.length:3;
 	for(var i=0;i<t;i++){
